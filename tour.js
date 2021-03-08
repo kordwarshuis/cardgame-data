0463f4f5f56943f1d71b3422bce66d29eb2d26d1
kor <kor.dwarshuis@gmail.com>
2021-03-08 15:14:20 +0100

    New file

---
+++ tour.js
@@ -0,0 +1,48 @@
+const tour = {
+  options: {
+    useKeyboardNavigation: false,
+    labels: {
+      buttonSkip: 'Skip tour',
+      buttonPrevious: 'Previous',
+      buttonNext: 'Next',
+      buttonStop: 'Finish'
+    }
+  },
+  steps: [{
+      target: '.navbar',
+      header: {
+        title: 'Welcome to the card game',
+      },
+      content: `Here you wil find misconceptions about Bitcoin.`
+    },
+    {
+      target: '.searchBar',
+      content: 'The Search is fast and immediate and searchs for exact strings of text. Give it a try, it‘s fun',
+      params: {
+        placement: 'top'
+      }
+    },
+    {
+      target: '.button-categories',
+      content: 'Choose a category here.',
+      params: {
+        placement: 'left'
+      }
+    },
+    {
+      target: '#card0',
+      content: 'Every card contains a misconception about Bitcoin.'
+    },
+    {
+      target: '.cards-show-all',
+      content: 'To see all cards, click on this button.'
+    },
+    {
+      target: '.twitter-open-close-handle',
+      header: {
+        title: 'Realtime tweets',
+      },
+      content: `Here you will find a live tweet stream about Bitcoin, that you can configure to your needs.`
+    }
+  ]
+};
