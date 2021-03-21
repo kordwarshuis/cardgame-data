26adaf8fca133977a6996d04535c6507867f8c35
kor <kor.dwarshuis@gmail.com>
2021-03-21 18:37:10 +0100

    Bots like google are now served a simple version of the csv content of the requested card

--- index-social-media-bcb.php
+++ index-social-media-bcb.php
@@ -1,28 +1,56 @@
 <?php
+
+// Configuration
+$pathCardGame = '/t/bcb/';
+$content = 'https://blockchainbird.org/t/data/cards.csv';
+
 // Configuration: location of images
-$path = '/t/media/img/social-media/system/';// also change .env file
+$path = '/t/media/img/social-media/system/'; // also change .env file
 $domain = 'https://blockchainbird.org';
-$defaultImage = 'https://blockchainbird.org/t/media/img/social-media/social-media-blockchain4.jpg';
+$defaultImage = 'https://blockchainbird.org/t/media/img/social-media/social-media-bitcoin8.jpg';
+$shortText = 'Bitcoin misconceptions.';
+$longText = 'Misconceptions about bitcoin hold back progress. Found a misconception? Lookup-Copy-Paste our permanent links with curated expert info!';
 
 // if specific image exists (image with url as name) then use that image, else use default image
-function check_file_exist($fullPath){
-  if (file_exists($fullPath)) {
-    return true;
-  } else {
-    return false;
-  }
+function check_file_exist($fullPath)
+{
+    if (file_exists($fullPath)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 // take the parameter with name 'url'
 $filename = $_GET['url'];
 
 $socialMediaImage = "";
-if (check_file_exist($_SERVER['DOCUMENT_ROOT'].$path.$filename.'.jpg')) {
-  $socialMediaImage = $domain.$path.$filename.'.jpg';
+if (check_file_exist($_SERVER['DOCUMENT_ROOT'] . $path . $filename . '.jpg')) {
+    $socialMediaImage = $domain . $path . $filename . '-text.jpg';
+    $twitterImage = $domain . $path . $filename . '.jpg';
 } else {
-  $socialMediaImage = $defaultImage;
+    $socialMediaImage = $defaultImage;
+    $twitterImage = $defaultImage;
+}
+
+// https://phpenthusiast.com/blog/parse-csv-with-php
+// The nested array to hold all the arrays
+$cardContent = [];
+
+// Open file
+if (($h = fopen("{$content}", "r")) !== false) {
+    // Each line in the file is converted into an individual array that we call $data
+    // The items of the array are comma separated
+    while (($data = fgetcsv($h, 1000, ",")) !== false) {
+        // Each individual array is being pushed into the nested array
+        $cardContent[] = $data;
+    }
+
+    // Close the file
+    fclose($h);
 }
 ?>
+
 
 <!DOCTYPE html>
 <html lang=en>
@@ -31,31 +59,51 @@ if (check_file_exist($_SERVER['DOCUMENT_ROOT'].$path.$filename.'.jpg')) {
   <meta charset=utf-8>
   <meta http-equiv=X-UA-Compatible content="IE=edge">
   <meta name=viewport content="width=device-width,initial-scale=1">
-  <title>Bitcoin Misconceptions.</title>
+  <title><?php echo $shortText; ?></title>
   <meta name=viewport content="width=device-width,initial-scale=1">
-  <meta name=description
-    content="Misconceptions about bitcoin hold back progress. Found a misconception? Lookup-Copy-Paste our permanent links with curated expert info!">
-  <meta name=keywords
-    content="Misconceptions about bitcoin hold back progress. Found a misconception? Lookup-Copy-Paste our permanent links with curated expert info!">
+  <meta name=description content="<?php echo $longText; ?>">
+  <meta name=keywords content="<?php echo $longText; ?>">
   <meta name=author content="Blockchain Bird">
   <!-- <meta property=og:image itemprop=image content=https://blockchainbird.org/t/cardgametest/img-twitter/twitter.jpg> <meta -->
-  <meta property=og:image itemprop=image content=<?php echo $socialMediaImage;?>>
+  <meta property=og:image itemprop=image content=<?php echo $socialMediaImage; ?>>
   <meta property=og:image:width content=1200>
   <meta property=og:image:height content=628>
-  <meta property=og:title content="Bitcoin Misconceptions.">
-  <meta property=og:description
-    content="Misconceptions about bitcoin hold back progress. Found a misconception? Lookup-Copy-Paste our permanent links with curated expert info!">
-  <meta property=og:site_name content="Bitcoin Misconceptions.">
+  <meta property=og:title content="<?php echo $shortText; ?>">
+  <meta property=og:description content="<?php echo $longText; ?>">
+  <meta property=og:site_name content="<?php echo $shortText; ?>">
   <meta property=og:type content=website>
   <meta name=twitter:card content=summary_large_image>
-  <meta name=twitter:title content="Bitcoin Misconceptions.">
-  <meta name=twitter:description
-    content="Misconceptions about bitcoin hold back progress. Found a misconception? Lookup-Copy-Paste our permanent links with curated expert info!">
-  <meta name=twitter:image content=<?php echo $socialMediaImage;?>>
-  <meta itemprop=image content=<?php echo $socialMediaImage;?>>
+  <meta name=twitter:title content="<?php echo $shortText; ?>">
+  <meta name=twitter:description content="<?php echo $longText; ?>">
+  <meta name=twitter:image content=<?php echo $twitterImage; ?>>
+  <meta itemprop=image content=<?php echo $twitterImage; ?>>
 </head>
 
 <body>
+
+<?php
+
+// find where the Unique URL is
+// select the first row
+$row0 = $cardContent[0];
+for ($i = 0; $i < count($row0); $i++) {
+    if ($row0[$i] == 'Unique URL') {
+        $arrayPositionUniqueURL = $i;
+    }
+}
+
+foreach ($cardContent as $key => $value) {
+    if (!is_array($value)) {
+        echo $value . "\r\n";
+    } else {
+        foreach ($value as $key2 => $value2) {
+            if ($value[$arrayPositionUniqueURL] == $filename && $value2 != '') {
+                echo "<p>" . $value2 . "</p>\r\n";
+            }
+        }
+    }
+}
+?>
 </body>
 
 </html>
