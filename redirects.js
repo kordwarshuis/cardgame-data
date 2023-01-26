a7ab01efedae8ac60e13410469ffd3f240765767
kor <kor@dwarshuis.com>
2023-01-26 08:30:25 +0100

    https://blockchainbird.com/t/btc to https://btc.blockchainbird.org/

--- redirects.js
+++ redirects.js
@@ -1,103 +1,103 @@
 // redirects
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Architecture&q=preview-7-transactions-per-second&title=Bitcoin%20has%20only%20seven%20transactions%20per%20second') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/7-transactions-per-second');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Architecture&q=preview-7-transactions-per-second&title=Bitcoin%20has%20only%20seven%20transactions%20per%20second') {
+  window.location.replace('https://btc.blockchainbird.org/card/7-transactions-per-second');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Beliefs&q=preview-convince-me&title=You%20cannot%20convince%20me%20of%20the%20usefulness%20of%20bitcoin') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/convince-me');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Beliefs&q=preview-convince-me&title=You%20cannot%20convince%20me%20of%20the%20usefulness%20of%20bitcoin') {
+  window.location.replace('https://btc.blockchainbird.org/card/convince-me');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Beliefs&q=preview-too-late&title=I%E2%80%98ve%20already%20missed%20out%20on%20bitcoin') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/too-late');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Beliefs&q=preview-too-late&title=I%E2%80%98ve%20already%20missed%20out%20on%20bitcoin') {
+  window.location.replace('https://btc.blockchainbird.org/card/too-late');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Control&q=preview-chinese-government&title=Bitcoin%20is%20controlled%20by%20the%20Chinese%20government') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/chinese-government');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Control&q=preview-chinese-government&title=Bitcoin%20is%20controlled%20by%20the%20Chinese%20government') {
+  window.location.replace('https://btc.blockchainbird.org/card/chinese-government');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Control&q=preview-few-deciders&title=Only%20a%20few%20people%20have%20ultimate%20control%20over%20Bitcoin') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/few-deciders');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Control&q=preview-few-deciders&title=Only%20a%20few%20people%20have%20ultimate%20control%20over%20Bitcoin') {
+  window.location.replace('https://btc.blockchainbird.org/card/few-deciders');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Control&q=preview-quantum-computing&title=Quantum%20computers%20will%20breach%20the%20security%20of%20crypto-currencies') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/quantum-computing');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Control&q=preview-quantum-computing&title=Quantum%20computers%20will%20breach%20the%20security%20of%20crypto-currencies') {
+  window.location.replace('https://btc.blockchainbird.org/card/quantum-computing');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Crime&q=preview-anonymous&title=Bitcoin%20is%20anonymous') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/anonymous');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Crime&q=preview-anonymous&title=Bitcoin%20is%20anonymous') {
+  window.location.replace('https://btc.blockchainbird.org/card/anonymous');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Crime&q=preview-anonymous&title=Bitcoin%20is%20anonymous%2C%20criminals%20go%20unpunished') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/anonymous');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Crime&q=preview-anonymous&title=Bitcoin%20is%20anonymous%2C%20criminals%20go%20unpunished') {
+  window.location.replace('https://btc.blockchainbird.org/card/anonymous');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Crime&q=preview-anonymous&title=Bitcoin%20is%20anonymous,%20criminals%20go%20unpunished') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/anonymous');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Crime&q=preview-anonymous&title=Bitcoin%20is%20anonymous,%20criminals%20go%20unpunished') {
+  window.location.replace('https://btc.blockchainbird.org/card/anonymous');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Crime&q=preview-criminals&title=Bitcoin%20is%20mostly%20for%20criminals') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/criminals');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Crime&q=preview-criminals&title=Bitcoin%20is%20mostly%20for%20criminals') {
+  window.location.replace('https://btc.blockchainbird.org/card/criminals');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Crime&q=preview-money-laundering&title=Bitcoin%20is%20one%20big%20money%20laundering%20machine') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/money-laundering');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Crime&q=preview-money-laundering&title=Bitcoin%20is%20one%20big%20money%20laundering%20machine') {
+  window.location.replace('https://btc.blockchainbird.org/card/money-laundering');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Crime&q=preview-tax-evaders&title=Bitcoin%20is%20for%20tax%20evaders') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/tax-evaders');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Crime&q=preview-tax-evaders&title=Bitcoin%20is%20for%20tax%20evaders') {
+  window.location.replace('https://btc.blockchainbird.org/card/tax-evaders');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Environment&q=preview-complex-useless-calcs&title=Complex%2C%20useless%20calculations%20soak%20up%20energy') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/complex-useless-calcs');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Environment&q=preview-complex-useless-calcs&title=Complex%2C%20useless%20calculations%20soak%20up%20energy') {
+  window.location.replace('https://btc.blockchainbird.org/card/complex-useless-calcs');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Environment&q=preview-energy-for-elsewhere&title=Sustainable%20energy%20should%20be%20used%20elsewhere') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/energy-for-elsewhere');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Environment&q=preview-energy-for-elsewhere&title=Sustainable%20energy%20should%20be%20used%20elsewhere') {
+  window.location.replace('https://btc.blockchainbird.org/card/energy-for-elsewhere');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Environment&q=preview-transaction-electricity&title=Per%20transaction%2C%20bitcoin%20costs%20XX%20MegaWatts%2C%20YY%20euros') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/transaction-electricity');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Environment&q=preview-transaction-electricity&title=Per%20transaction%2C%20bitcoin%20costs%20XX%20MegaWatts%2C%20YY%20euros') {
+  window.location.replace('https://btc.blockchainbird.org/card/transaction-electricity');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Environment&q=preview-unsustainable&title=Bitcoin%20is%20unsustainable%20and%20killing%20the%20earth') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/unsustainable');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Environment&q=preview-unsustainable&title=Bitcoin%20is%20unsustainable%20and%20killing%20the%20earth') {
+  window.location.replace('https://btc.blockchainbird.org/card/unsustainable');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Existential&q=preview-intrinsic-value&title=Bitcoin%20has%20no%20intrinsic%20value') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/intrinsic-value');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Existential&q=preview-intrinsic-value&title=Bitcoin%20has%20no%20intrinsic%20value') {
+  window.location.replace('https://btc.blockchainbird.org/card/intrinsic-value');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Existential&q=preview-not-first-crypto&title=Bitcoin%20was%20not%20the%20first%20cryptocurrency') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/not-first-crypto');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Existential&q=preview-not-first-crypto&title=Bitcoin%20was%20not%20the%20first%20cryptocurrency') {
+  window.location.replace('https://btc.blockchainbird.org/card/not-first-crypto');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Existential&q=preview-ponzi&title=Bitcoin%20is%20a%20Ponzi%20scheme,%20a%20new%20tulip%20mania') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/ponzi');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Existential&q=preview-ponzi&title=Bitcoin%20is%20a%20Ponzi%20scheme,%20a%20new%20tulip%20mania') {
+  window.location.replace('https://btc.blockchainbird.org/card/ponzi');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Existential&q=preview-ponzi&title=Bitcoin%20is%20a%20Ponzi%20scheme%2C%20a%20new%20tulip%20mania') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/ponzi');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Existential&q=preview-ponzi&title=Bitcoin%20is%20a%20Ponzi%20scheme%2C%20a%20new%20tulip%20mania') {
+  window.location.replace('https://btc.blockchainbird.org/card/ponzi');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Existential&q=preview-will-die&title=Bitcoin%20will%20die') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/will-die');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Existential&q=preview-will-die&title=Bitcoin%20will%20die') {
+  window.location.replace('https://btc.blockchainbird.org/card/will-die');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Legal&q=preview-legal-tender&title=Bitcoin%20is%20not%20legal%20tender') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/legal-tender');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Legal&q=preview-legal-tender&title=Bitcoin%20is%20not%20legal%20tender') {
+  window.location.replace('https://btc.blockchainbird.org/card/legal-tender');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Legal&q=preview-shut-down&title=Governments%20will%20shut%20bitcoin%20down') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/shut-down');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Legal&q=preview-shut-down&title=Governments%20will%20shut%20bitcoin%20down') {
+  window.location.replace('https://btc.blockchainbird.org/card/shut-down');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Price&q=preview-cant-buy-1&title=I%20can') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/cant-buy-anything');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Price&q=preview-cant-buy-1&title=I%20can') {
+  window.location.replace('https://btc.blockchainbird.org/card/cant-buy-anything');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Price&q=preview-price-criminals&title=A%20high%20bitcoin%20price%20is%20due%20to%20criminals%E2%80%98%20involvement') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/price-criminals');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Price&q=preview-price-criminals&title=A%20high%20bitcoin%20price%20is%20due%20to%20criminals%E2%80%98%20involvement') {
+  window.location.replace('https://btc.blockchainbird.org/card/price-criminals');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Price&q=preview-price-criminals&title=High%20bitcoin%20price%20is%20due%20to%20criminals') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/price-criminals');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Price&q=preview-price-criminals&title=High%20bitcoin%20price%20is%20due%20to%20criminals') {
+  window.location.replace('https://btc.blockchainbird.org/card/price-criminals');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Price&q=preview-price-down&title=The%20bitcoin%20price%20went%20down%20lately') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/price-down');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Price&q=preview-price-down&title=The%20bitcoin%20price%20went%20down%20lately') {
+  window.location.replace('https://btc.blockchainbird.org/card/price-down');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Skills&q=preview-its-over-after-21&title=After%20the%2021%20milion%20bitcoin%20are%20mined%2C%20it%E2%80%98s%20over') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/its-over-after-21');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Skills&q=preview-its-over-after-21&title=After%20the%2021%20milion%20bitcoin%20are%20mined%2C%20it%E2%80%98s%20over') {
+  window.location.replace('https://btc.blockchainbird.org/card/its-over-after-21');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Supply&q=preview-inflation&title=Bitcoin%20has%20strong%20inflation') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/inflation');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Supply&q=preview-inflation&title=Bitcoin%20has%20strong%20inflation') {
+  window.location.replace('https://btc.blockchainbird.org/card/inflation');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Supply&q=preview-volatile&title=Bitcoin%20is%20volatile') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/volatile');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Supply&q=preview-volatile&title=Bitcoin%20is%20volatile') {
+  window.location.replace('https://btc.blockchainbird.org/card/volatile');
 }
-if (window.location.href === 'https://blockchainbird.com/t/btc/?cat=Use&q=preview-slow&title=Bitcoin%20is%20slow') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/slow');
+if (window.location.href === 'https://btc.blockchainbird.org/?cat=Use&q=preview-slow&title=Bitcoin%20is%20slow') {
+  window.location.replace('https://btc.blockchainbird.org/card/slow');
 }
-if (window.location.href === 'https://www.blockchainbird.com/t/btc/?cat=Beliefs&q=preview-too-late&title=I%E2%80%98ve%20already%20missed%20out%20on%20bitcoin') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/too-late');
+if (window.location.href === 'https://www.blockchainbird.org/t/btc/?cat=Beliefs&q=preview-too-late&title=I%E2%80%98ve%20already%20missed%20out%20on%20bitcoin') {
+  window.location.replace('https://btc.blockchainbird.org/card/too-late');
 }
-if (window.location.href === 'https://www.blockchainbird.com/t/btc/?cat=Environment&q=preview-proof-of-stake&title=Proof-of-stake%20coins%20are%20far%20more%20sustainable%20than%20mining%20bitcoin') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/proof-of-stake');
+if (window.location.href === 'https://www.blockchainbird.org/t/btc/?cat=Environment&q=preview-proof-of-stake&title=Proof-of-stake%20coins%20are%20far%20more%20sustainable%20than%20mining%20bitcoin') {
+  window.location.replace('https://btc.blockchainbird.org/card/proof-of-stake');
 }
-if (window.location.href === 'https://www.blockchainbird.com/t/btc/card/energy-for-elsewhere') {
-  window.location.replace('https://blockchainbird.com/t/btc/card/energy-use-elsewhere');
+if (window.location.href === 'https://www.blockchainbird.org/t/btc/card/energy-for-elsewhere') {
+  window.location.replace('https://btc.blockchainbird.org/card/energy-use-elsewhere');
 }
